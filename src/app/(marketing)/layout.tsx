8d4851aa1fc2067b4d8a6caafc0b9dc87eaeb6b2
kelvinempire <kelvinewurum@gmail.com>
2025-02-26 17:07:28 +0100

    initialize project with Next.js, Tailwind CSS, and Clerk authentication

---
+++ src/app/(marketing)/layout.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from "react";
+import NavBar from "./_components/NavBar";
+
+export default function marketingLayout({children}:{ 
+    children: ReactNode }) {
+  return (
+<div className="selection:bg-[hsl(320,65%,52%,20%)]">
+      <NavBar />
+      {children}
+    </div>
+  );
+}
